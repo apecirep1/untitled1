df6e379af4c9c1e88c8b26d9a89c74b28fe4a7f3
Andrej Pecirep <apecirep1@etf.unsa.ba>
2020-10-26 21:49:07 +0100

    initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Ne radi nista, sampo proba");
+    }
+}
+EVO TI ERRORA U DESNOM DONJEM UGLU!!~
